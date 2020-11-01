8774d42edf95667847d3f42b8d5fb495a92a82f3
kramlex <mark.dubkov@gmail.com>
2020-11-01 20:48:09 +0700

    refactoring

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const BacklightFunction = (on = true) => {
+const backlightFunction = (on = true) => {
     const selectors = document.querySelectorAll('*')
     const selector = selectors[getRandomInt(0,selectors.length - 1)]
-    selector.style.boxShadow = on ? `0px -1px 8px 6px rgba(${getRandomInt(0,255)}, ${getRandomInt(0,255)}, ${getRandomInt(0,255)}, ${Math.random()})` : ``
+    selector.style.boxShadow = on ? `0px -1px 8px 8px rgba(${getRandomInt(0,255)}, ${getRandomInt(0,255)}, ${getRandomInt(0,255)}, ${Math.random()})` : ``
 }
 
-BacklightFunction()
+backlightFunction()
 
-setInterval(BacklightFunction,2000,)
-setInterval(BacklightFunction,2000,false)
+let selectInterval = setInterval(backlightFunction,2000)
+let unselectInterval = setInterval(backlightFunction,2000,false)
